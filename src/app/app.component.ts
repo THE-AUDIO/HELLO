55eebe441e8d79fc74b8d71fb98bbe6f68bb7c3a
THE-AUDIO <nandraina.dev22@gmail.com>
2025-05-07 12:31:26 +0300

    dans la bonne voie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,109 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
+import { gsap } from 'gsap'
+import { ScrollSmoother, ScrollTrigger, SplitText } from 'gsap/all';
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger, SplitText)
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  constructor(
+    @Inject(PLATFORM_ID) private platFormId: Object
+  ){}
+
+  @ViewChild('hello') hello!: ElementRef;
+  @ViewChild('hello0') hello0!: ElementRef;
+  @ViewChild('world0') world0!: ElementRef;
+  @ViewChild('line') line !: ElementRef
+  @ViewChild('boxes') boxes !: ElementRef
+  texteAnimation(classe:string){
+    let split = SplitText.create(classe, {
+      type:"chars, words, lines"  
+    })
+    gsap.from(split.chars,{
+      y:100,
+      autoAlpha:0,
+      stagger:{
+        amount:0.5,
+        from:"random"
+      },
+      scrollTrigger:{
+        trigger: classe,
+        start:"center 85%",
+        end:"top 70%",
+        scrub:1
+      }
+    })
+  }
+  ngAfterViewInit(): void {
+    if(isPlatformBrowser(this.platFormId)){
+    const spans = this.hello.nativeElement.querySelectorAll('span')
+    gsap.set(spans, {opacity: 0, y:20 });
+    let t1 = gsap.timeline();
+    t1.to(this.line.nativeElement,{
+        duration:.5,
+        height:"100%",
+    })
+    t1.to(this.line.nativeElement,{
+      width:"100%",
+    })
+    t1.to(this.hello0.nativeElement,{
+      duration:.5,
+      marginTop:0,
+      opacity:1,
+      color:"#fdbc00"
+    })
+    t1.to(this.world0.nativeElement,{
+      duration:.5,
+      marginTop:0,
+      opacity:1
+    })
+
+    gsap.timeline().to(spans, {
+      y:0,
+      opacity:1,
+      stagger: 1,
+      duration: 3,
+      ease: "back",
+      scrollTrigger:{
+        trigger:spans,
+        start:"center 65%",
+        end:"top 50%",
+        scrub:1,
+      }
+    })
+    let smoother =  ScrollSmoother.create({
+        wrapper:"#wrapper",
+        content:"#content",
+        effects: true,
+      })
+
+      smoother.effects("img",{speed:"auto"})
+      let split = SplitText.create('.texte4', {
+        type:"chars"  
+      })
+      gsap.from(split.chars,{
+        y:100,
+        autoAlpha:0,
+        stagger:{
+          amount:0.5,
+          from:"random"
+        },
+        scrollTrigger:{
+          trigger: ".texte4",
+          start:"center 70%",
+          end:"top 50%",
+          scrub:1,
+        }
+      })
+      this.texteAnimation('.texte')
+      this.texteAnimation('.texte2')
+      this.texteAnimation('.texte3')
+     
+    }
+  }
   title = 'HELLO';
 }
